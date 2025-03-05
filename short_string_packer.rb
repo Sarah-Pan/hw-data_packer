7e49cf99b14c1411271bcc2ceddad912390c6bba
Sarah-Pan <sarah2950@gmail.com>
2025-03-05 18:29:37 +0800

    Enter meaningful description here

--- short_string_packer.rb
+++ short_string_packer.rb
@@ -7,7 +7,12 @@ module ShortStringPacker
   #   str - String object
   # Returns: a Integer object
   def self.pack(str)
-    # IMPLEMENT THIS METHOD
+    packed = 0
+    str.chars.each do |char|
+      v = char.ord - 'a'.ord + 1
+      packed = packed << 5 | v
+    end
+    packed
   end
 
   ## Unpacks a Integer from pack() method into a short string
@@ -15,6 +20,15 @@ module ShortStringPacker
   #   packed - a Integer object
   # Returns: a String object
   def self.unpack(packed)
-    # IMPLEMENT THIS METHOD
+    return '' if packed.zero?
+
+    # Calculate the number of iterations
+    iterations = (packed.bit_length + 4) / 5
+    # Use each_with_index for controlled iteration
+    (0...iterations).each_with_index.inject('') do |ori_str, (_, _index)|
+      v = packed & 31
+      char = ('a'.ord + v - 1).chr
+      char + ori_str.tap { packed >>= 5 }
+    end
   end
 end
